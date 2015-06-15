e67ba9b650deb5100e070d853276eb6c4898b053
Dan Kwon <dankwon@Dans-Air-2.home>
2015-06-15 14:01:09 -0400

    added api calls for adding insights and deleting profiles

--- staging/app/profiles.js
+++ staging/app/profiles.js
@@ -5,9 +5,11 @@ app.controller('ProfilesController', ['$scope', '$http', function($scope, $http)
 	$scope.currentUser = {'loggedIn':'no'};
 	$scope.featuredProfiles = null;
 	$scope.selectedProfile = {'firstName':'', 'lastName':'', 'age':'', 'city':'', 'country':'', 'bio':''}; // insert empty values so angular doesn't freak out
+	$scope.insight = {'description':'', 'category':''};
+
 	$scope.ageArray = new Array();
-	$scope.root = 'http://86.terranga-org.appspot.com';
-	$scope.testing = true;
+	$scope.root = 'http://89.terranga-org.appspot.com';
+	$scope.testing = false;
 	
 	$scope.init = function(){
 		console.log('Profiles Controller: INIT');
@@ -49,6 +51,75 @@ app.controller('ProfilesController', ['$scope', '$http', function($scope, $http)
 		console.log(JSON.stringify($scope.selectedProfile));
 	}
 
+	$scope.viewInsights = function(index){
+		$scope.selectedProfile = $scope.featuredProfiles[index];
+		console.log(JSON.stringify($scope.selectedProfile));
+	}
+	
+	$scope.deleteProfile = function(index){
+		var profile = $scope.featuredProfiles[index];
+		
+		var t = confirm('Are You Sure? This Cannot Be Undone.');
+		if (t==false){
+			console.log('NO!');
+			return;
+		}
+		
+		console.log('YES!');
+		
+		var path = '/api/profiles/'+profile.id;
+    	var url = ($scope.testing==true) ? $scope.root+path : path;
+    	
+        $http.delete(url).success(function(data, status, headers, config) {
+            var confirmation = data['confirmation'];
+            console.log('CONFIRMATION: '+JSON.stringify(data));
+
+            if (confirmation != 'success'){
+                alert(data['message']);
+                return;
+            }
+            
+            $scope.featuredProfiles.splice(index, 1);
+            
+        }).error(function(data, status, headers, config) {
+            console.log("error", data, status, headers, config);
+        });
+		
+	}
+
+	
+	$scope.addInsight = function(){
+		if ($scope.selectedProfile==null)
+			return;
+		
+		$scope.insight['profile'] = $scope.selectedProfile.id;
+		var json = JSON.stringify($scope.insight);
+		console.log('ADD INSIGHT: '+json);
+
+		var path = '/api/insights';
+    	var url = ($scope.testing==true) ? $scope.root+path : path;
+    	
+        $http.post(url, json).success(function(data, status, headers, config) {
+            var confirmation = data['confirmation'];
+            console.log('CONFIRMATION: '+JSON.stringify(data));
+
+            if (confirmation != 'success'){
+                alert(data['message']);
+                return;
+            }
+            
+            $scope.selectedProfile.insights.unshift(data['insight']);
+        	$scope.insight = {'description':'', 'category':''}; // clear the insight
+            
+            
+        }).error(function(data, status, headers, config) {
+            console.log("error", data, status, headers, config);
+        });
+		
+		
+	}
+	
+	
 	$scope.updateSelectedProfile = function(){
 		if ($scope.selectedProfile==null)
 			return;
