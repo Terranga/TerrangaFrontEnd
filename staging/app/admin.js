de6fa3c80e46a47c9e5ddd4054dcf5bec8f440d4
bkarpinos <bkarpinos@gmail.com>
2015-06-23 18:03:13 -0400

    complete initial functionality of admin console

--- staging/app/admin.js
+++ staging/app/admin.js
@@ -3,8 +3,14 @@ var app = angular.module('AdminModule', []);
 
 app.controller('AdminController', ['$scope', '$http', function($scope, $http){
 	
-	$scope.possibleChoices = ['Profile View A', 'Profile View B'];
+	$scope.possibleChoices = ['profile_view_A', 'profile_view_B'];
 	$scope.profileChoice = null;
+	
+	
+	$scope.profilePages = null;
+	
+	
+	$scope.profilePage = {'page':'profile_view_A'};
 
 
 	
@@ -20,9 +26,72 @@ app.controller('AdminController', ['$scope', '$http', function($scope, $http){
 	}
 	
 	$scope.update = function(){
+		var url = '/api/profilePage';
+		 $http.get(url).success(function(data, status, headers, config) {
+			 var confirmation = data['confirmation'];
+	         console.log('CONFIRMATION: '+JSON.stringify(data));
+	         
+	         if (confirmation != 'success'){
+	        	 alert(data['message']);
+	        	 return;
+	         }
+	         
+	         if (data['profilePages'] != null)
+	        	 $scope.profilePages = data['profilePages'];
+	         
+	         $scope.profilePage = $scope.profilePages[0];
+	         $scope.profilePage['page']= $scope.profileChoice;
+	         
+	         var url = '/api/profilePage/'+$scope.profilePage['id'];
+	     	
+	         var json = JSON.stringify($scope.profilePage);
+	         
+	         
+	         $http.put(url, json).success(function(data, status, headers, config) {
+	             var confirmation = data['confirmation'];
+	             console.log('CONFIRMATION: '+JSON.stringify(data));
+
+	             if (confirmation != 'success'){
+	                 alert(data['message']);
+	                 return;
+	             }
+	             
+	             alert('Profile Updated');
+	             
+	         }).error(function(data, status, headers, config) {
+	             console.log("error", data, status, headers, config);
+	         });
+
+
+	            
+	     }).error(function(data, status, headers, config) {
+	    	 console.log("error", data, status, headers, config);
+	     });
+		
 		console.log('Send Jake: '+ $scope.profileChoice);
-		alert('New Profile View: '+ $scope.profileChoice);
+		
 	}
+	
+	
+	
+	$scope.createProfilePage = function(){
+		var json = JSON.stringify($scope.profilePage);
+    	var url = '/api/profilePage';
+        $http.post(url, json).success(function(data, status, headers, config) {
+            var confirmation = data['confirmation'];
+            console.log('CONFIRMATION: '+confirmation);
+            
+            if (confirmation != 'success'){
+                alert(data['message']);
+                return;
+            }
+            
+            alert('ProfilePage Created')
+        }).error(function(data, status, headers, config) {
+            console.log("error", data, status, headers, config);
+        });
+	}
+	
 	
 
 }]);
